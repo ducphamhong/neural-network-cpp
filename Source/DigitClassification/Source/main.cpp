9791396d151f922909d8ea8a07bf33bf56c482a0
Pham Hong Duc <hongduc.pr@gmail.com>
2023-03-18 15:44:46 +0700

    Add XOR Test

--- Source/DigitClassification/Source/main.cpp
+++ Source/DigitClassification/Source/main.cpp
@@ -1,6 +1,15 @@
 #include "pch.h"
+#include "ANN.h"
 
 int main()
 {
+	// input: 774
+	// hidden: 32
+	// output: 10
+	int dim[] = { 784, 32, 10 };
+
+	// init 3 layers
+	ANN::CANN ann(dim, 3);
+
 	return 1;
 }
---
+++ Source/XOR/Source/main.cpp
@@ -0,0 +1,61 @@
+#include "pch.h"
+#include "ANN.h"
+
+int main()
+{
+	// input: 2
+	// hidden: 4
+	// output: 10
+	int dim[3] = { 2,4,1 };
+
+	// train data
+	double input[] = {
+		0,0,
+		0,1,
+		1,0,
+		1,1
+	};
+
+	double output[] = {
+		0,
+		1,
+		1,
+		0
+	};
+
+	// init 3 layers
+	ANN::CANN ann(dim, 3);
+
+	ann.LearnExpected = [](double* trainData, int trainId, int neuralOutputId) {
+		// XOR will return 0, 1 for neuralOutputId
+		return trainData[trainId];
+	};
+
+	ann.Predict = [](const ANN::SLayer& output)
+	{
+		// Convert output layer to XOR result
+		if (output.Neurals[0].Output >= 0.5)
+			return 1;
+		else
+			return 0;
+	};
+
+	// train
+	printf("Begin learning\n");
+	for (int i = 0; i < 10000; i++)
+	{
+		ann.train(input, output, 4);
+	}
+
+	// test learning
+	printf("Result\n");
+	double* p = input;
+	for (int i = 0; i < 4; i++)
+	{
+		double ret = ann.predict(p);
+		printf("%d xor %d = %d\n", (int)p[0], (int)p[1], (int)ret);
+		p += 2;
+	}
+
+	return 1;
+}
